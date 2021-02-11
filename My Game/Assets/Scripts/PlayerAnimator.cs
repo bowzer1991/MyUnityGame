50d24ed31fbe250c19e6365fdee7c5765f00d7cd
Joe Bowers <bowers_1991@hotmail.co.uk>
2021-02-11 00:50:12 +0000

    Played with wavey terrain in one scene, worked on a new character with CharacterController component, working on 1D animation as he runs around

---
+++ My Game/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator anim;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontalVelocity = controller.velocity;
+        horizontalVelocity = new Vector3(controller.velocity.x, 0, controller.velocity.z);
+        float overallSpeed = controller.velocity.magnitude;
+        anim.SetFloat("velocity", overallSpeed);
+
+        Debug.Log(controller.velocity.magnitude);
+    }
+}
